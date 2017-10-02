d9ce9c0a4c3f0635f0d88a85818922c0a7c5b2c6
Vinod Joshi <vinojos@microsoft.com>
2017-10-02 12:58:06 +0530

    Add project files.

---
+++ DeveloperExperienceVS/Model/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperExperienceVS.Model
+{
+    public class Employee
+    {
+        private string name = "Vinod";
+
+        public string GetEmployeeName()
+        {
+            return name;
+        }
+    }
+}
